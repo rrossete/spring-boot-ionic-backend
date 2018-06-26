e37ba39b71ac2baa05cffb9979cf170673809e20
Rodolpho Rossete <rodolpho@ice.ufjf.br>
2018-06-26 11:27:54 -0300

    Criação Categoria Domain

--- src/main/java/br/ufjf/projeto/resources/CategoriaResources.java
+++ src/main/java/br/ufjf/projeto/resources/CategoriaResources.java
@@ -1,16 +1,26 @@
 package br.ufjf.projeto.resources;
 
+import br.ufjf.projeto.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
 
-        return "Rest esta funcionando!";
+        List<Categoria> lista = new ArrayList<>();
+
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 }
