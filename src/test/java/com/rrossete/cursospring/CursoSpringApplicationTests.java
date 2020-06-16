f113e605a3d2b4bc58a2c64825ef2f7fa26ca007
Rodolpho Rossete <rodolpho@ice.ufjf.br>
2020-06-16 13:28:54 -0300

    Refazendo o curso do zero

---
+++ src/test/java/com/rrossete/cursospring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrossete.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
